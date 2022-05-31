a0c39ad3f4c0cc58bde454d67c7067ac6c980890
Nicholas Estelami <NEstelami@users.noreply.github.com>
2022-05-31 00:59:35 -0400

    RGBA8 CVar Support Added

--- libultraship/libultraship/Cvar.h
+++ libultraship/libultraship/Cvar.h
@@ -2,6 +2,7 @@
 #define _CVAR_H
 
 #include <PR/ultra64/gbi.h>
+#include "color.h"
 
 #ifdef __cplusplus
 typedef enum class CVarType 
@@ -20,6 +21,7 @@ typedef struct CVar {
         s32 valueS32;
         float valueFloat;
         const char* valueStr;
+        Color_RGBA8 valueRGBA;
     } value;
 } CVar;
 
@@ -32,15 +34,19 @@ extern "C"
 #endif
 //#include <ultra64.h>
 
-CVar* CVar_Get(const char* name);
 s32 CVar_GetS32(const char* name, s32 defaultValue);
 float CVar_GetFloat(const char* name, float defaultValue);
 const char* CVar_GetString(const char* name, const char* defaultValue);
 void CVar_SetS32(const char* name, s32 value);
 
+Color_RGB8 CVar_GetRGB(const char* name, Color_RGB8 defaultValue);
+Color_RGBA8 CVar_GetRGBA(const char* name, Color_RGBA8 defaultValue);
+void CVar_SetRGBA(const char* name, Color_RGBA8 value);
+
 void CVar_RegisterS32(const char* name, s32 defaultValue);
 void CVar_RegisterFloat(const char* name, float defaultValue);
 void CVar_RegisterString(const char* name, const char* defaultValue);
+void CVar_RegisterRGBA(const char* name, Color_RGBA8 defaultValue);
 
 #ifdef __cplusplus
 };
@@ -52,6 +58,7 @@ void CVar_RegisterString(const char* name, const char* defaultValue);
 #include <functional>
 #include <memory>
 
+extern "C" CVar * CVar_Get(const char* name);
 extern std::map<std::string, std::unique_ptr<CVar>, std::less<>> cvars;
 void CVar_SetFloat(const char* name, float value);
 void CVar_SetString(const char* name, const char* value);
